22735d1d71bbcc347f96f61a50db986608056c12
sumxtx <137871537+sumxtx@users.noreply.github.com.>
2024-10-30 17:38:54 +0200

    more methods added

--- Arrays/ArraysUtil.java
+++ Arrays/ArraysUtil.java
@@ -89,6 +89,50 @@ public class ArraysUtil
 		}
 		return max;
 	}
+	public int secondMaximum(int arr[])
+	{
+		int max = Integer.MIN_VALUE;
+		int secondMax = Integer.MIN_VALUE;
+		for(int i = 0; i < arr.length; i++)
+		{
+			if(arr[i] > max)
+			{
+				secondMax = max;
+				max = arr[i];
+			}
+			else if(arr[i] > secondMax && arr[i] != max)
+			{
+				secondMax = arr[i];
+			}
+		}
+		return secondMax;
+	}
+	public void moveElements(int arr[], int l, int n)
+	{
+		int j = 0;
+		for(int i = 0; i < l; i++)
+		{
+			if(arr[i] != n && arr[j] == n)
+			{
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+			if(arr[j] != n)
+			{
+				j++;
+			}
+		}
+	}
+	public void expandArray_buggy(int arr[], int capacity)
+	{
+		int temp[] = new int[capacity];
+		for(int i = 0; i < arr.length; i++)
+		{
+			temp[i] = arr[i];
+		}
+		arr = temp;
+	}
 	public static void main(String args[])
 	{
 		int myArray[] = new int[]{1,2,45,0,0,32,0,4,59,6,70,3};
@@ -97,33 +141,66 @@ public class ArraysUtil
 		//Print an Array
 		System.out.println("Array:");
 		arrUtil.printArray(myArray);
+		System.out.println();
 
 		// Remove Even numbers from an Array
 		System.out.println("Odd Array");
 		int oddArray[] = arrUtil.removeEven(myArray);
 		arrUtil.printArray(oddArray);
+		System.out.println();
 
 		// Remove Odds numbers from an Array
 		System.out.println("Even Array");
 		int eveArray[] = arrUtil.removeOdds(myArray);
 		arrUtil.printArray(eveArray);
+		System.out.println();
 
 		//Reverse the Array
 		System.out.println("Array in Reverse Order");
 		arrUtil.reverseArray(myArray, 0, myArray.length -1);
 		arrUtil.printArray(myArray);
+		arrUtil.reverseArray(myArray, 0, myArray.length -1);
+		arrUtil.printArray(myArray);
+		System.out.println();
 
 		//Find the Mininum number of an Array
 		System.out.println("Array Mininum Number");
 		int min = arrUtil.findMininum(myArray);
 		System.out.println(min);
+		System.out.println();
 
 		//Find the Maximum number of an Array
 		System.out.println("Array Maximum Number");
 		int max = arrUtil.findMaximum(myArray);
 		System.out.println(max);
+		System.out.println();
 		
+		//Find the Maximum number of an Array
+		System.out.println("Second Array Maximum Number");
+		int secmax = arrUtil.secondMaximum(myArray);
+		System.out.println(secmax);
+		System.out.println();
 
+		// Copy the Array To a new one
+		//Move all Matching Elements to the end of an Array	
+		int ntoMove = 0;
+		System.out.println("Move all matching elements to the end of the Array");
+		System.out.println("Move all zeros to the end of an Array");
+		int copyArr[] = new int[myArray.length];
+		for(int i = 0; i < myArray.length; i++)
+		{
+			copyArr[i] = myArray[i];
+		}
+		arrUtil.printArray(copyArr);
+		arrUtil.moveElements(copyArr, copyArr.length, ntoMove);
+		arrUtil.printArray(copyArr);
+		System.out.println();
+
+		//Resize an Array - Buggy (Garbage Collected as they call it here)
+		int l_myArray= myArray.length;
+		System.out.println("Size of the Array before expand: "+ l_myArray);
+		arrUtil.expandArray_buggy(myArray, 20);
+		System.out.println("Size of the Array after expand: "+ l_myArray);
 		
 	}
 }
