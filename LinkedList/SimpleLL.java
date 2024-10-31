cb1d3e4ea0ffa5f8ad4dbd4009a51dabcd4a8af4
sumxtx <137871537+sumxtx@users.noreply.github.com.>
2024-10-31 17:01:53 +0100

    Added more methods, claculate length, add first, add last, remove first, elements of ll

--- LinkedList/SimpleLL.java
+++ LinkedList/SimpleLL.java
@@ -23,6 +23,53 @@ public class SimpleLL
 		}
 		System.out.println("null");
 	}
+	public int length()
+	{
+		if(head == null)
+		{
+			return (0);
+		}
+		int count = 0;
+		ListNode current = head;
+		while(current != null)
+		{
+			count++;
+			current = current.next;
+		}
+		return count;
+	}
+	public void insertFirst(int value)
+	{
+		ListNode newNode = new ListNode(value);
+		newNode.next = head;
+		head = newNode;
+	}
+	public void insertLast(int value)
+	{
+		ListNode newNode = new ListNode(value);
+		if(head == null)
+		{
+			head = newNode;
+			return;
+		}
+		ListNode current = head;
+		while(null != current.next)
+		{
+			current = current.next;
+		}
+		current.next = newNode;
+	}
+	public ListNode deleteFirst()
+	{
+		if(head == null)
+		{
+			return null;
+		}
+		ListNode temp = head;
+		head = head.next;
+		temp.next = null;
+		return temp;
+	}
 	public static void main(String args[])
 	{
 		SimpleLL sll = new SimpleLL();
@@ -35,6 +82,28 @@ public class SimpleLL
 		second.next = third;
 		third.next = fourth;
 
+		// Print the Elements of the list
 		sll.display();
+
+		// Print the amount of elements in the list
+		System.out.println("List length: " + sll.length());
+
+		// Insert Element at the Beginning of the list
+		sll.insertFirst(89);
+		sll.display();
+		System.out.println("List length: " + sll.length());
+		sll.insertFirst(890);
+		sll.display();
+		System.out.println("List length: " + sll.length());
+
+		// Insert Elements at the end of the list
+		sll.insertLast(7890);
+		sll.display();
+		System.out.println("List length: " + sll.length());
+
+		// Delete the first element of a list
+		sll.deleteFirst();
+		sll.display();
+		System.out.println("List length: " + sll.length());
 	}
 }
